dd1fa2eac7e56cb6cabd899866044e8dcd566da2
JIJIHUNY <dlwlgns2792@gmail.com>
2024-08-14 16:32:25 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="chatqa",
+    version="0.1.0",
+    packages=find_packages(include=["./src/*"]),
+    python_requires=">=3.11.0",
+    install_requires=[
+        "overrides",
+        "transformers",
+        "datasets",
+        "evaluate",
+        "accelerate",
+        "trl",
+        "peft",
+        "bitsandbytes",
+        "wandb",
+    ],
+    entry_points={
+        "console_scripts": ["chatqa-eval=src.eval:main", "chatqa-train=src.train:main"]
+    },
+)
