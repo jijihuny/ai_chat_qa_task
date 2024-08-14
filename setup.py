0bb90ec05fd9ac7d3e687fd13a4a97d9bc809012
JIJIHUNY <dlwlgns2792@gmail.com>
2024-08-14 22:57:34 +0900

    fix version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         "overrides",
         "transformers",
-        "datasets",
+        "datasets==2.20.0",
         "evaluate",
         "accelerate",
         "trl",
